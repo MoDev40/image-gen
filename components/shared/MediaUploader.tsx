76c58ec3576564336d972a8a73613905aac8a12f
M40 <modev.404@gmail.com>
2024-06-05 13:40:27 +0300

    🔧 Update MediaUploader to handle new Cloudinary response structure

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,33 +1,34 @@
-import { Dispatch, SetStateAction } from "react";
-import { Image as ImageInterface } from "./AddTranFormationForm";
 import { CldImage, CldUploadWidget } from 'next-cloudinary';
 import Image from "next/image";
+import { Dispatch, SetStateAction } from "react";
 import { useToast } from "../ui/use-toast";
+import { Image as ImageInterface } from "./AddTranFormationForm";
 
 interface MediaUploaderProps {
-    secureURL: string;
     setImage:Dispatch<SetStateAction<ImageInterface>>;
     onValueChange:(value:string) => void;
+    imageURL:string;
     publicId: string;
 }
-function MediaUploader({publicId,secureURL,setImage,onValueChange}: MediaUploaderProps) {
+function MediaUploader({publicId,imageURL,setImage,onValueChange}: MediaUploaderProps) {
   const { toast } = useToast()
 
   function handleSuccess(result:any) {
     setImage((previous)=> ({
         ...previous,
-        width: result.info.width,
-        height: result.info.height,
-        secureURL: result.info.secure_url,
+        width: result?.info?.width,
+        height: result?.info?.height,
+        secureURL: result?.info?.secure_url,
+        publicId:result?.info?.public_id
     }))
 
-    onValueChange(result.info.publicId)
+    onValueChange(result?.info?.public_id)
 
     toast({
       title: "Image Uploaded",
-      description: "Image uploaded successfully",
+      description: "Image uploaded successfully 1 credit has been used", 
       duration: 5000,
-      className:"bg-green-400"
+      className:"bg-green-400 text-white"
     })
   }
 
@@ -36,7 +37,7 @@ function MediaUploader({publicId,secureURL,setImage,onValueChange}: MediaUploade
       title: "Image Upload Failed",
       description: "Image upload failed unexpectedly",
       duration: 5000,
-      className:"bg-red-400"
+      className:"bg-red-400 text"
     })
 
   }
@@ -44,19 +45,28 @@ function MediaUploader({publicId,secureURL,setImage,onValueChange}: MediaUploade
     <CldUploadWidget
     onSuccess={handleSuccess}
     onError={handleError} 
-    uploadPreset="<Your Upload Preset>">
+    uploadPreset="ImageGen"
+    options={{
+      multiple:false,
+      resourceType:"image"
+    }}>
       {({ open }) =>(
-        <div>
+        <div className="flex flex-col gap-4">
+          <h3 className="h3-bold text-dark-600">
+            Original
+          </h3>
           {
             publicId ?
-            <div>
-              <CldImage
-                src={secureURL}
+            <div className="cursor-pointer overflow-hidden rounded-[10px]">
+            <CldImage
+                src={publicId || imageURL}
+                width={250}
+                height={200}
                 alt="image/uploaded"
               />
             </div> :
             <div className="flex flex-col justify-center items-center h-auto bg-white shadow p-5 rounded">
-              <button onClick={() => open()}>
+              <button type="button" onClick={() => open()}>
                 <Image
                 src='/assets/icons/add.svg'
                 width={20}
