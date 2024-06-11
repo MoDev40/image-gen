842540dddb15c512d3ffd6c57a990cc7eed79e5c
M40 <modev.404@gmail.com>
2024-06-11 17:26:45 +0300

    ✨ Add DBUser type and implement GET route for fetching user by clerkId.

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,3 +6,18 @@ declare type CreateUser = {
     firstName: string;
     lastName: string;
 }
+
+type DBUser = {
+    _id: string;
+    clerkId: string;
+    email: string;
+    username: string;
+    photo: string;
+    firstName: string;
+    lastName: string;
+    planId: number;
+    creditBalance: number;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+}
