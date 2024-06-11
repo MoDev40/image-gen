969ea7f0b15bb831ecf660713c19f09893d867e8
M40 <modev.404@gmail.com>
2024-06-11 17:27:31 +0300

    ✨ Add feature to fetch user data on AddTransformationType page.

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,8 @@
-import Header from '@/components/shared/Header'
-import React from 'react'
-import { transformationTypes } from '@/constants'
 import AddTranFormationForm from '@/components/shared/AddTranFormationForm';
+import Header from '@/components/shared/Header';
+import { transformationTypes } from '@/constants';
+import { auth } from '@clerk/nextjs/server';
+import axios from 'axios';
 
 interface Params {
   type:string
@@ -16,13 +17,21 @@ interface TransformationType {
   };
   icon: string;
 }
-function AddTransformationType({params}:{params:Params}) {
+
+async function getUser (id:string) : Promise<DBUser> {
+  const res = await axios.get(`http://localhost:3000/api/users/${id}`);
+  return await res.data.user;
+}
+
+async function AddTransformationType({params}:{params:Params}) {
   const {type} = params
+  const { userId } : { userId: string | null } = auth();
+  const user = await getUser(userId!)
   const transform : TransformationType = transformationTypes[type as keyof typeof transformationTypes]
   return (
     <>
     <Header title={transform.title} subtitle={transform.subTitle}/>
-    <AddTranFormationForm type={transform.type}/>
+    <AddTranFormationForm user={user}  type={transform.type}/>
     </>
   )
 }
