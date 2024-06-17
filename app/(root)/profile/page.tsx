40ccf9aee9dcacd5f607aa2245d7e079b93c6622
M40 <modev.404@gmail.com>
2024-06-17 16:22:57 +0300

    ✨ Add Profile component and getProfileData function & update loading.

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Header from '@/components/shared/Header';
+import Profile from '@/components/shared/Profile';
+import { API } from '@/lib/config';
+import { auth } from '@clerk/nextjs/server';
+import axios from 'axios';
 
-function ProfilePage() {
+export async function getProfileData (id:string) : Promise<ProfileInterface> {
+  const res = await axios.get(`${API}/users/${id}/profile`);
+  return await res.data;
+}
+
+async function ProfilePage() {
+  const { userId } : { userId: string | null } = auth();
+  const data = await getProfileData(userId!)
   return (
-    <div>ProfilePage</div>
+    <>
+    <Header title='Profile'/>
+    <Profile creditBalance={data.creditBalance} imageManipulated={data.imageManipulated}/>
+    </>
   )
 }
 
